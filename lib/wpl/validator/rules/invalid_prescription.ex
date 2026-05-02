6490ab638963a00bb2c4f7754f27020f55b1bca6
Alex Filatov <alex@alexfilatov.com>
2026-05-02 21:49:51 +0100

    feat(rule): INVALID_PRESCRIPTION (TDD: 7 tests)

--- lib/wpl/validator/rules/invalid_prescription.ex
+++ lib/wpl/validator/rules/invalid_prescription.ex
@@ -1,4 +1,96 @@
 defmodule WPL.Validator.Rules.InvalidPrescription do
   @moduledoc false
   use WPL.Validator.Rule
+
+  alias WPL.Validator.{Error, WalkContext}
+
+  @valid_types MapSet.new(["sets_reps", "time", "distance", "amrap", "continuous", "intervals"])
+
+  @impl true
+  def enter_activity(ctx, activity, path) do
+    if Map.get(activity, "type") == "exercise" do
+      check_prescription(ctx, activity, path)
+    else
+      ctx
+    end
+  end
+
+  defp check_prescription(ctx, activity, path) do
+    prescription = Map.get(activity, "prescription")
+
+    if is_map(prescription) do
+      validate_prescription(ctx, prescription, "#{path}/prescription")
+    else
+      ctx
+    end
+  end
+
+  defp validate_prescription(ctx, p, pres_path) do
+    case Map.fetch(p, "type") do
+      :error ->
+        WalkContext.emit(ctx, %Error{
+          path: pres_path,
+          code: :invalid_prescription,
+          message: "prescription missing 'type'",
+          severity: :error,
+          meta: %{reason: :missing_type}
+        })
+
+      {:ok, t} when is_binary(t) ->
+        if MapSet.member?(@valid_types, t) do
+          ctx
+          |> check_sets_reps(p, t, pres_path)
+          |> check_time(p, t, pres_path)
+        else
+          WalkContext.emit(ctx, %Error{
+            path: pres_path,
+            code: :invalid_prescription,
+            message: "unknown prescription type '#{t}'",
+            severity: :error,
+            meta: %{reason: :unknown_type, prescription_type: t}
+          })
+        end
+
+      {:ok, t} ->
+        WalkContext.emit(ctx, %Error{
+          path: pres_path,
+          code: :invalid_prescription,
+          message: "unknown prescription type '#{t}'",
+          severity: :error,
+          meta: %{reason: :unknown_type, prescription_type: t}
+        })
+    end
+  end
+
+  defp check_sets_reps(ctx, p, "sets_reps", pres_path) do
+    if Map.get(p, "sets") == nil and Map.get(p, "reps") == nil do
+      WalkContext.emit(ctx, %Error{
+        path: pres_path,
+        code: :invalid_prescription,
+        message: "sets_reps prescription requires 'sets' or 'reps'",
+        severity: :error,
+        meta: %{reason: :sets_reps_requires_sets_or_reps}
+      })
+    else
+      ctx
+    end
+  end
+
+  defp check_sets_reps(ctx, _p, _type, _path), do: ctx
+
+  defp check_time(ctx, p, "time", pres_path) do
+    if Map.get(p, "duration") == nil do
+      WalkContext.emit(ctx, %Error{
+        path: pres_path,
+        code: :invalid_prescription,
+        message: "time prescription requires 'duration'",
+        severity: :error,
+        meta: %{reason: :time_requires_duration}
+      })
+    else
+      ctx
+    end
+  end
+
+  defp check_time(ctx, _p, _type, _path), do: ctx
 end
---
+++ test/wpl/validator/rules/invalid_prescription_test.exs
@@ -0,0 +1,98 @@
+defmodule WPL.Validator.Rules.InvalidPrescriptionTest do
+  use ExUnit.Case, async: true
+
+  alias WPL.Validator.Rules.InvalidPrescription
+  alias WPL.Validator.{Error, WalkContext}
+
+  defp run_on_activity(activity) do
+    ctx = %WalkContext{}
+    path = "/plan/phases/0/weeks/0/days/0/blocks/0/activities/0"
+    InvalidPrescription.enter_activity(ctx, activity, path).errors |> Enum.reverse()
+  end
+
+  defp presc_path, do: "/plan/phases/0/weeks/0/days/0/blocks/0/activities/0/prescription"
+
+  describe "InvalidPrescription.enter_activity/3" do
+    test "flags sets_reps prescription with neither sets nor reps" do
+      activity = %{
+        "id" => "a1",
+        "type" => "exercise",
+        "exercise_ref" => "push_up",
+        "prescription" => %{"type" => "sets_reps"}
+      }
+
+      errors = run_on_activity(activity)
+      assert length(errors) == 1
+
+      assert hd(errors) == %Error{
+               path: presc_path(),
+               code: :invalid_prescription,
+               message: "sets_reps prescription requires 'sets' or 'reps'",
+               severity: :error,
+               meta: %{reason: :sets_reps_requires_sets_or_reps}
+             }
+    end
+
+    test "flags time prescription without duration" do
+      activity = %{
+        "id" => "a1",
+        "type" => "exercise",
+        "exercise_ref" => "plank",
+        "prescription" => %{"type" => "time"}
+      }
+
+      errors = run_on_activity(activity)
+      assert length(errors) == 1
+      assert hd(errors).meta.reason == :time_requires_duration
+    end
+
+    test "flags missing prescription type" do
+      activity = %{
+        "id" => "a1",
+        "type" => "exercise",
+        "exercise_ref" => "push_up",
+        "prescription" => %{"sets" => 3, "reps" => 10}
+      }
+
+      errors = run_on_activity(activity)
+      assert length(errors) == 1
+      assert hd(errors).meta.reason == :missing_type
+    end
+
+    test "flags unknown prescription type" do
+      activity = %{
+        "id" => "a1",
+        "type" => "exercise",
+        "exercise_ref" => "push_up",
+        "prescription" => %{"type" => "forever"}
+      }
+
+      errors = run_on_activity(activity)
+      assert length(errors) == 1
+      err = hd(errors)
+      assert err.meta.reason == :unknown_type
+      assert err.meta.prescription_type == "forever"
+    end
+
+    test "does not flag valid sets_reps prescription" do
+      activity = %{
+        "id" => "a1",
+        "type" => "exercise",
+        "exercise_ref" => "push_up",
+        "prescription" => %{"type" => "sets_reps", "sets" => 3, "reps" => 10}
+      }
+
+      assert run_on_activity(activity) == []
+    end
+
+    test "does not fire on non-exercise activity types" do
+      activity = %{"id" => "a1", "type" => "simple", "name" => "walk"}
+      assert run_on_activity(activity) == []
+    end
+
+    test "does not fire when prescription is absent" do
+      activity = %{"id" => "a1", "type" => "exercise", "exercise_ref" => "push_up"}
+      assert run_on_activity(activity) == []
+    end
+  end
+end
